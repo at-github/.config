49076cdecae5bae32d4ec99ce6abb1f52305ad11
Tarik <tarikamar@gmail.com>
2022-10-11 14:36:07 +0000

    Neovim - Add it as submodule
    WIP a lot of things to work

--- install.sh
+++ install.sh
@@ -20,3 +20,24 @@ if ! [ -x "$(command -v python)" ]; then
   echo "Install python for almost the prompt"
   sudo apt install python-is-python3
 fi
+
+# Neovim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+# TODO get neovim > 0.7
+sudo apt install ripgrep fd-find python3-pip npm
+
+pip3 install pynvim
+
+sudo npm install -g neovim eslint
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '55ce33d7678c5a611085589f1f3ddf8b3c52d662cd01d4ba75c0ee0459970c2200a51f492d557530c71c15d8dba01eae') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
+sudo mv composer.phar /usr/local/bin/composer
+
+nvim +PackerSync
+nvim +PackerCompile
