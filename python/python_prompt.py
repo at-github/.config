0d2cd5af150908edda89c67851e772b627dbdafb
Tarik Amar <tarik.amar@gmail.com>
2024-02-12 14:04:20 +0100

    Python - Add python icon to the prompt

--- python/python_prompt.py
+++ python/python_prompt.py
@@ -2,5 +2,5 @@
 import sys
 
 # https://www.codementor.io/@arpitbhayani/personalize-your-python-prompt-13ts4kw6za
-sys.ps1 = "\033[1;33m➜\033[0m "
+sys.ps1 = "\033[1;33m \ue73c ➜\033[0m "
 sys.ps2 = "\033[1;34m…\033[0m "
